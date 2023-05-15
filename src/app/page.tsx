f02bff9069b97cf9a49d2f6dd40dfe046fa3f8f5
Gabriel Oliveira <gabrieloliveira.gho@gmail.com>
2023-05-15 12:38:38 -0300

    feat: basic initial test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,29 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
+import {walkOverData} from "../mock/simple-data";
+import {Match, SingleEliminationBracket, SVGViewer} from "@g-loot/react-tournament-brackets";
+import useSWR from "swr";
+import {useEffect} from "react";
 
 export default function Home() {
+    const { data, error, isLoading } = useSWR('/api/user',
+        fetch('https://api.challonge.com/v1/tournaments/12858808.json?api_key=qo3OH0FVeZiNeisI0qrincgrrip6gwqv8j0peA2R')
+        .then((res) => res.json()))
+
+    useEffect(() => { console.log(data)}, [data]);
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <SingleEliminationBracket
+          matches={walkOverData}
+          matchComponent={Match}
+          svgWrapper={({ children, ...props }) => (
+              <SVGViewer width={1200} height={700} {...props}>
+                {children}
+              </SVGViewer>
+          )}
+      />
     </main>
   )
 }
